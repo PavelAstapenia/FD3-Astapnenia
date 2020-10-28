225ced92ab6140010652bc3a6b40cda3ce5c098b
Pavel_A <ip_astap@mail.ru>
2020-10-28 13:05:44 +0300

    HW1 draft

---
+++ ishop/IshopItemsBlock.js
@@ -0,0 +1,35 @@
+let StockBlockDiv = React.createClass({
+
+    displayName: 'StockBlock',
+
+    getDefaultProps: function () {
+        return { question: "No data available" }
+    },
+
+    render: function () {
+        let itemCode =
+            React.DOM.tr({ className: 'tableRow' },
+                React.DOM.th({ className: 'columnName' }, "Name"),
+                React.DOM.th({ className: 'columnPrice' }, "Price"),
+                React.DOM.th({ className: 'columnPhoto' }, "Photo"),
+                React.DOM.th({ className: 'columnStock' }, "Stock"));
+
+        let itemsCode = this.props.items.reduce((prevVal, current) => {
+
+            prevVal.push(
+                React.DOM.tr({ key: current.itemCode, className: 'tableRow' },
+                    React.DOM.th({ className: 'columnName' }, current.name),
+                    React.DOM.th({ className: 'columnPrice' }, current.price),
+                    React.DOM.th({ className: 'columnPhoto' },
+                        React.DOM.a({ className: 'photoURL', target: "_blank", href: current.photoURL }, "Photo")),
+                    React.DOM.th({ className: 'columnStock' }, current.stock))
+            );
+            return prevVal;
+        }, [itemCode]);
+
+        return React.DOM.div({ className: 'StockBlock' },
+            React.DOM.div({ className: 'ShopName' }, this.props.shopName),
+            React.DOM.table({ className: 'Items', border: 1 }, itemsCode),
+        );
+    },
+});
