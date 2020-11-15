6f55d56e4b8d21a98a21a3a5b28ccb55ca580e91
Pavel_A <ip_astap@mail.ru>
2020-11-15 09:19:43 +0300

    ishop2 ver1

--- ishop2/ItemBlock.js
+++ ishop2/ItemBlock.js
@@ -26,12 +26,19 @@ let ItemBlock = React.createClass({
     },
 
     selectRow: function (EO) {
-        console.log(EO.target.dataset.id);
-        console.log(EO.target.getAttribute('data-id'));
+        console.log('data ' + EO.target.getAttribute('data-id'));
+        let n = EO.target.getAttribute('data-id');
+        if (n !== null) {
+            this.setState((prevState) => { return { itemSelected: prevState.itemSelected = n } });
+        };
+        console.log('state' + this.state.itemSelected);
     },
 
     deletItem: function (EO) {
-        this.props.cbDeletItem(2);
+        console.log('data-id-del ' + EO.target.getAttribute('data-id-del'));
+        let n = EO.target.getAttribute('data-id-del');
+        this.props.cbDeletItem(+n);
+
     },
 
     render: function () {
@@ -42,13 +49,13 @@ let ItemBlock = React.createClass({
 
             prevVal.push(
                 React.DOM.tr({ key: current.itemCode, className: name, onClick: this.selectRow, 'data-id': current.itemCode },
-                    React.DOM.th({ className: 'columnName' }, current.name),
-                    React.DOM.th({ className: 'columnPrice' }, current.price),
-                    React.DOM.th({ className: 'columnURL' },
-                        React.DOM.a({ className: 'URL', target: "_blank", href: current.URL }, current.URL)),
-                    React.DOM.th({ className: 'columnStock' }, current.stock),
+                    React.DOM.th({ className: 'columnName', 'data-id': current.itemCode }, current.name),
+                    React.DOM.th({ className: 'columnPrice', 'data-id': current.itemCode }, current.price),
+                    React.DOM.th({ className: 'columnURL', 'data-id': current.itemCode },
+                        React.DOM.a({ className: 'URL', target: "_blank", href: current.URL, 'data-id': current.itemCode }, current.URL)),
+                    React.DOM.th({ className: 'columnStock', 'data-id': current.itemCode }, current.stock),
                     React.DOM.th({ className: 'columnControl' },
-                        React.DOM.input({ type: 'button', value: 'Delet', onClick: this.deletItem }))
+                        React.DOM.input({ type: 'button', value: 'Delet', onClick: this.deletItem, 'data-id-del': current.itemCode }))
                 )
             );
             return prevVal;
