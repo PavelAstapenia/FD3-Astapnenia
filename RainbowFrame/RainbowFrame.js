83098bf49b6b887ada3a96fe5d2b9a0408cfa7f4
Pavel_A <ip_astap@mail.ru>
2020-12-02 10:13:26 +0300

    RainbowFrame 1.0.0

---
+++ RainbowFrame/RainbowFrame.js
@@ -0,0 +1,20 @@
+﻿import React from 'react';
+import PropTypes from 'prop-types';
+
+function RainbowFrame(props) {
+  let arr = props.colors;
+  let arr1 = arr.slice(0, arr.length - 1);
+  return (<React.Fragment>
+
+
+    <div className="divColorFrame" style={{ border: "solid 7px " + arr[arr.length - 1], padding: "5px", display: "inline-flex" }} >
+      {(arr.length > 1) ? <RainbowFrame colors={arr1}>
+        {props.children}
+      </RainbowFrame> :
+        <div style={{ padding: "10px 80px", display: "inline-flex" }}>{<b>{props.children}</b>}</div>}
+    </div>
+
+  </React.Fragment >);
+};
+
+export default RainbowFrame;
---
+++ br2jsx/br2jsx.js
@@ -0,0 +1,14 @@
+﻿import React from 'react';
+import PropTypes from 'prop-types';
+
+import './br2jsx.css';
+
+function BR2JSX(props) {
+  let text = props.text;
+  let text1 = text.match(/[a-zA-Zа-яА-ЯёЁ]+/gu).map(elem => (elem == 'br') ? <br /> : elem);
+  return (<div className='br2jsx'>
+    {text1}
+  </div >);
+};
+
+export default BR2JSX;
