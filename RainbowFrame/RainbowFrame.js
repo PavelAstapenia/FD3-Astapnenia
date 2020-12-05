128e0e701918390c04441439fe409bdeed1a921e
Pavel_A <ip_astap@mail.ru>
2020-12-05 12:16:14 +0300

    1.0.1 из рекурсии убрал реакт компонент

--- RainbowFrame/RainbowFrame.js
+++ RainbowFrame/RainbowFrame.js
@@ -3,18 +3,20 @@ import PropTypes from 'prop-types';
 
 function RainbowFrame(props) {
   let arr = props.colors;
-  let arr1 = arr.slice(0, arr.length - 1);
-  return (<React.Fragment>
+  let child = props.children;
 
+  function makeRainbow(arr, child) {
+    return (<React.Fragment>
 
-    <div className="divColorFrame" style={{ border: "solid 7px " + arr[arr.length - 1], padding: "5px", display: "inline-flex" }} >
-      {(arr.length > 1) ? <RainbowFrame colors={arr1}>
-        {props.children}
-      </RainbowFrame> :
-        <div style={{ padding: "10px 80px", display: "inline-flex" }}>{<b>{props.children}</b>}</div>}
-    </div>
+      <div className="divColorFrame" style={{ border: "solid 7px " + arr[arr.length - 1], padding: "5px", display: "inline-flex" }} >
+        {(arr.length > 1) ? makeRainbow((arr.slice(0, arr.length - 1)), child) :
+          <div style={{ padding: "10px 80px", display: "inline-flex" }}>{<b>{child}</b>}</div>}
+      </div>
 
-  </React.Fragment >);
+    </React.Fragment >);
+  }
+
+  return makeRainbow(arr, child);
 };
 
 export default RainbowFrame;
