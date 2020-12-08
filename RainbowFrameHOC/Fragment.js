84c161541edd562d0d977f1a4533d445542366c2
Pavel_A <ip_astap@mail.ru>
2020-12-08 12:46:57 +0300

    RainbowFrameHOC 1.0.1 DoubleButton added

--- RainbowFrameHOC/Fragment.js
+++ RainbowFrameHOC/Fragment.js
@@ -1,8 +1,30 @@
 ﻿import React from 'react';
 import PropTypes from 'prop-types';
 
-function Fragment(props) {
-  return (<div style={{ padding: "10px 80px", display: "inline-flex" }}>{<b>{props.children}</b>}</div>);
-};
+class DoubleButton extends React.Component {
 
-export default Fragment;
+  static PropTypes = {
+    caption1: PropTypes.string.isRequired,
+    caption2: PropTypes.string.isRequired,
+    cbPressed: PropTypes.func.isRequired,
+  }
+
+  pressedBtn = (EO) => {
+    this.props.cbPressed(EO.target.value);
+  }
+
+  render() {
+    return (
+      <div style={{ padding: "10px 80px", display: "inline-flex" }}>
+        <input className='Btn' type='button' value={this.props.caption1} onClick={this.pressedBtn} />
+        { <b>{this.props.children}</b>}
+        <input className='Btn' type='button' value={this.props.caption2} onClick={this.pressedBtn} />
+      </div >
+    );
+
+  }
+
+}
+
+export default DoubleButton;
+
--- RainbowFrameHOC/app.js
+++ RainbowFrameHOC/app.js
@@ -3,15 +3,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import Fragment from './Fragment';
+import DoubleButton from './Fragment';
 import { withRainbowFrame } from './withRainbowFrame';
 
 let colors = ['red', 'orange', 'yellow', 'green', '#00BFFF', 'blue', 'purple'];
-let FramedFragment = withRainbowFrame(colors)(Fragment);
+let FramedDoubleButton = withRainbowFrame(colors)(DoubleButton);
 
 ReactDOM.render(
-    <FramedFragment>
-        Hello!
-    </FramedFragment>
+    <div style={{ display: "inline-grid" }}>
+        <DoubleButton caption1="однажды" caption2="пору" cbPressed={num => alert(num)}>
+            в студёную зимнюю
+        </DoubleButton>
+
+
+        <FramedDoubleButton caption1="я из лесу" caption2="мороз" cbPressed={num => alert(num)}>
+            вышел, был сильный
+        </FramedDoubleButton>
+    </div>
     , document.getElementById('container')
 );
